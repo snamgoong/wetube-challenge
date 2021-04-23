4e067483842f56524b788f32f800589976a142f9
NGS-OSS <77262459+snamgoong@users.noreply.github.com>
2021-04-23 11:09:52 +0900

    Update videoController.js

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,7 @@
 
 import routes from "../routes";
 import Video from "../models/Video";
+import Comment from "../models/Comment";
 
 export const home = async(req, res) => {
   try{
@@ -55,8 +56,9 @@ export const videoDetail = async(req, res) => {
     params: {id}
   } = req;
   try{
-  const video = await (await Video.findById(id)).populate("creator");
-  console.log(video);
+     const video = await Video.findById(id)
+       .populate("creator")
+       .populate("comments");
   res.render("videoDetail", {pageTitle: video.title, video});
   } catch (error) {
     res.redirect(routes.home);
@@ -121,6 +123,26 @@ export const postRegisterView = async (req, res) => {
      video.views += 1;
      video.save();
      res.status(200);
+   } catch (error) {
+     res.status(400);
+   } finally {
+     res.end();
+   }
+ };
+export const postAddComment = async (req, res) => {
+   const {
+     params: { id },
+     body: { comment },
+     user
+   } = req;
+   try {
+     const video = await Video.findById(id);
+     const newComment = await Comment.create({
+       text: comment,
+       creator: user.id
+     });
+     video.comments.push(newComment.id);
+     video.save();
    } catch (error) {
      res.status(400);
    } finally {
